10e46997281559a2194bdf38983f496b12ba7665
Ronen <97254@LAPTOP-C8H59LP1>
2021-03-21 12:03:12 +0200

    first

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class SharedData 
 {
-	private ArrayList <Integer> array;
+	private int[] array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
